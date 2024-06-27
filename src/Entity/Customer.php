74bf392b364273e60fb0f5d9523b6eaaf7a3faaf
Sylvain Vandermeersch <sylvain.vandermeersch@gmail.com>
2024-06-27 19:53:13 +0200

    add groups to avoid circular reference

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -7,6 +7,7 @@ namespace App\Entity;
 use App\Repository\CustomerRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Doctrine\DBAL\Types\Types;
 use App\Validator\Constraints as CustomAssert;
 
@@ -17,34 +18,40 @@ class Customer
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: Types::INTEGER)]
+    #[Groups(['getCustomerDetails'])]
     private ?int $id = null;
 
     #[ORM\Column(type: Types::STRING, length: 255)]
     #[Assert\NotBlank]
     #[Assert\Length(min: 2, max: 50)]
     #[Assert\Regex(pattern:"/^[a-zA-ZÀ-ÿ '-]+$/u", message:"Votre nom ne peut contenir que des lettres.")]
+    #[Groups(['getCustomerDetails'])]
     private ?string $firstName = null;
 
     #[ORM\Column(type: Types::STRING, length: 255)]
     #[Assert\NotBlank]
     #[Assert\Length(min: 2, max: 50)]
     #[Assert\Regex(pattern:"/^[a-zA-ZÀ-ÿ '-]+$/u", message:"Votre nom ne peut contenir que des lettres.")]
+    #[Groups(['getCustomerDetails'])]
     private ?string $lastName = null;
 
     #[ORM\Column(type: Types::STRING, length: 255, unique: true)]
     #[Assert\NotBlank]
     #[Assert\Length(min: 6, max: 100)]
     #[Assert\Email]
+    #[Groups(['getCustomerDetails'])]
     private ?string $email = null;
 
     #[ORM\Column(type: Types::DATETIME_IMMUTABLE)]
     #[Assert\NotBlank]
     #[Assert\LessThanOrEqual(value: "today", message: "La date de création ne peut pas être dans le futur.")]
+    #[Groups(['getCustomerDetails'])]
     private ?\DateTimeImmutable $createdAt = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     #[Assert\NotBlank]
     #[Assert\LessThanOrEqual(value: "today", message: "La date de mise à jour ne peut pas être dans le futur.")]
+    #[Groups(['getCustomerDetails'])]
     private ?\DateTime $updatedAt = null;
 
     #[ORM\ManyToOne(inversedBy: 'customers')]
